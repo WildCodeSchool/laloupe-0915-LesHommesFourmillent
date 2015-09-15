bf41e932778a359cb60c2d4c42ef5342b209fa73
AlexB44 <a.bodier130@gmail.com>
2015-09-15 17:26:02 +0200

    Biggest commit

--- navbar.php
+++ navbar.php
@@ -1,8 +1,10 @@
 
-  <ul class="navbar">
-    <li><a href="#">Intélligence collective</a></li>
-    <li><a href="#">ngéniérie sociale</a></li>
-    <li><a href="#">Espace numérique</a></li>
-    <li><a href="#">Développement durable</a></li>
+  <ul class="nav">
+  	<li><img src="imgs/logopetit.jpg" height=200px></li>
+    <li><a href="#"><img src="imgs/bouton_ic.jpg" height=100px alt="Intélligence collective>"></a></li>
+    <li><a href="#"><img src="imgs/bouton_is.jpg" height=100px alt="Ingéniérie sociale>"></a></li>
+    <li><a href="#"><img src="imgs/bouton_solidarite.jpg" height=100pxx alt="Solidarité"></a></li>
+    <li><a href="#"><img src="imgs/bouton_en.jpg" height=100px alt="Espace numérique"></a></li>
+    <li><a href="#"><img src="imgs/bouton_dd.jpg" height=100px alt="Développement durable"></a></li>
   </ul>
 
