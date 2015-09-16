6c9b62db3bac5cc089178e4010ffb6e8c3ed7c0c
EnteJulien <julien.ente@gmail.com>
2015-09-16 09:26:32 +0200

    création d'une page test

---
+++ pagetest.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Page test</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/bootstrap.icon-large.min.css"/>
+ 	<!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"-->
+	<link rel="stylesheet" href="css/notre.css">
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<?php include("navbar.php"); ?>
+
+	<p>Test</p>
+	
+	<?php include("footer.php"); ?>
+
+</body>
+</html>
